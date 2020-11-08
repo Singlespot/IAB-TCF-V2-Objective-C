9210d7c371f42e2d4af7fb1f7125482a8db62712
Matthew Paletta <mattpaletta@gmail.com>
2020-11-08 20:01:26 +0000

    Temporary fixes

--- Package.swift
+++ Package.swift
@@ -4,11 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "IAB-TCF-V2",
-    platforms: [.iOS(.v9)],
     products: [
         .library(name: "IAB-TCF-V2", targets: ["IAB-TCF-V2-API"]),
-        .library(name: "IAB-TCF-V2-Static", type: .static, targets: ["IAB-TCF-V2-API"]),
-        .library(name: "IAB-TCF-V2-Dynamic", type: .dynamic, targets: ["IAB-TCF-V2-API"]),
+        .library(name: "IAB-TCF-V2-Utils", targets: ["IAB-TCF-V2-Utils"]),
     ],
     targets: [
         .target(name: "IAB-TCF-V2-API", dependencies: ["IAB-TCF-V2-Utils"], path: "Sources/API"),
